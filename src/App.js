f52bffab543936fe3f2c9af462b8e340fe157bac
HelenJojo <helen.jojomon@hcl.com>
2021-11-09 17:15:07 -0500

    first commit
    Signed-off-by: HelenJojo <helen.jojomon@hcl.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import CreateProjectButton from "./components/Project/CreateProjectButton";
+import AddProject from "./components/Project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
+import UpdateProject from "./components/Project/UpdateProject";
+import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
+import AddProjectTask from "./components/ProjectBoard/ProjectTask/AddProjectTask";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+
+          <Route exact path="/dashboard" component={Dashboard}></Route>
+          <Route exact path="/addProject" component={AddProject}></Route>
+
+          <Route
+            exact
+            path="/updateProject/:id"
+            component={UpdateProject}
+          ></Route>
+          <Route
+            exact
+            path="/projectBoard/:id"
+            component={ProjectBoard}
+          ></Route>
+          <Route
+            exact
+            path="/addProjectTask/:id"
+            component={AddProjectTask}
+          ></Route>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
